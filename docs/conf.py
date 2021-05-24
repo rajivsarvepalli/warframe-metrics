ffcc147dc5cd13cb9a531f5de1954c6885053a26
unknown <rajiv@sarvepalli.net>
2021-05-24 18:06:46 -0400

    fixed version nums

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import warframe_metrics
 project = "warframe-metrics"
 author = "Rajiv Sarvepalli"
 copyright = f"{datetime.now().year}, {author}"
-version = "0.2.2"
+version = "0.1.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
