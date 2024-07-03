4a3e9a3f22172f78929f2ec1ab939a3f94def6b9
Rajiv Sarvepalli <rajiv@ericavijay.net>
2024-07-02 22:37:42 -0400

    Update datetime_utils.py

--- src/warframe_metrics/utils/datetime_utils.py
+++ src/warframe_metrics/utils/datetime_utils.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 import datetime
 
-def hour_minute_second_microsecond(time_delta: datetime.timedelta) -> Dict
+def hour_minute_second_microsecond(time_delta: datetime.timedelta) -> Dict:
     """Converts a timedelta to a dictionary of hour, minute, second, and microsecond."""
     return {
         "hour": time_delta.seconds//3600,
